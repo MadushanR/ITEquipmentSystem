e01b12c9b05041a94d06367c2df3da70d463ee07
Rajendran Madushan <madushanrajendran@gmail.com>
2024-09-30 17:43:59 -0400

    added all listing pages

--- Assignment1/Assignment1/Models/Repository.cs
+++ Assignment1/Assignment1/Models/Repository.cs
@@ -3,9 +3,11 @@
     public static class Repository
     {
         private static List<Request> responses = new();
+        private static int nextId = 1;
         public static IEnumerable<Request> Responses => responses;
         public static void AddResponse(Request response)
         {
+            response.Id = nextId++;
             Console.WriteLine(response); responses.Add(response);
         }
     }
